4a6d6acf736cd154c29d8c1e7162d24504505e1f
Nate Tan <nktan93@gmail.com>
2019-03-06 05:58:52 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,32 +1,26 @@
 require 'pry'
 
 def welcome
-  # code #welcome here
   puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
   rand(1..11)
 end
 
 def display_card_total(card_total)
-  # code #display_card_total here
   puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
-  # code #prompt_user here
   puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
   gets.chomp
 end
 
 def end_game(card_total)
-  # code #end_game here
   puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
@@ -41,7 +35,6 @@ def invalid_command
 end
 
 def hit?(card_total)
-  # code hit? here
   prompt_user
   input = get_user_input
   if input == "s"
@@ -53,12 +46,7 @@ def hit?(card_total)
   card_total
 end
 
-#####################################################
-# get every test to pass before coding runner below #
-#####################################################
-
 def runner
-  # code runner here
   welcome
   card_total = initial_round
   until card_total >= 21
@@ -67,5 +55,3 @@ def runner
   end
   end_game(card_total)
 end
-
-binding.pry
